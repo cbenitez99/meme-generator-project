36435d80685ca1919b4ed89ccbafe662643c648b
Cristian Benitez <cbenitezbusiness9@gmail.com>
2021-06-26 00:53:44 -0500

    Added tags to html, refactored code, added css & JS

---
+++ scripts/index.js
@@ -0,0 +1,14 @@
+console.log("All Files Connected!")
+
+const memeImg = document.querySelector("#meme-img")
+//const ul = document.getElementById('memes-container')
+const reloadMemeBttn = document.getElementById('button')
+const upvoteCount = document.getElementById('upvotes')
+
+
+reloadMemeBttn.addEventListener('click', getMeme)
+
+memeImg.addEventListener
+
+getMeme()
+
--- scripts/services.js
+++ scripts/services.js
@@ -1,5 +1,26 @@
 const URL = "https://meme-api.herokuapp.com/gimme/me_irl"
 
 function getMeme() {
-    return fetch(URL)
-}
+    return fetch(URL, {
+        headers: {
+            "Accept" : "application/json"
+        }
+    })
+    .then(res => res.json())
+    .then(data => {
+        const meme = data.url
+        memeImg.src = meme
+        upvoteCount.innerHTML = data.ups + " People have upvoted this meme."
+    });
+}   
+
+// function postMeme(body) {
+//     return fetch(URL, {
+//         method: 'POST',
+//         headers: {
+//             "Accept" : "application/json"
+//         },
+//         body: JSON.stringify(body)
+//     })
+//     .then(res => res.json())
+// }
